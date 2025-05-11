9b757fe648ffed14a5fdff3bae2c5dca2e1211a6
Ross Fletcher <rossfletcher108@gmail.com>
2025-05-11 16:46:21 +0100

    feat: finished javascript to create accessible image gallery, and added relevant CSS

--- app.js
+++ app.js
@@ -1,43 +1,74 @@
-console.info("hii");
-// use your console as a testing tool
+console.info("Hello World");
+//use your console as your testing tool!
 
-// TODO: There are three main tasks to do:
-// - Store images data
-// - Create Thumbnail Images
-// - Create the full-screen images
+//TODO There are 3 main tasks to do:
 
-// Step 1: Store your images data in an array - you 100% need src and alt for this
+//Step 1: Store your images data
+//you can use local images or external images
+//for simplicity, it might be easier this time to use external images
+
+// Step 1: Store your images data
+// I stayed true to the example given by Manny for this, and did not add any additional info like srcSrc, width, height ect. I got quite confused with running this code and wanted to keep it as simple as possible.
 const images = [
   {
-    src: "url",
-    alt: "img description",
+    src: "assets/images/aleksandra-gencheva-e9z-JEG6BDo-unsplashmed.jpg",
+    alt: "meaningful description of my image",
+    // srcSet: ,
     // imgName: ,
-    // width: ,
-    // height: ,
+    // width:,
+    // height:
   },
-  { src: "url", alt: "img description" },
-  { src: "url", alt: "img description" },
+  {
+    src: "assets/images/medium qual example image.jpg",
+    alt: "meaningful description of my image",
+  },
+  {
+    src: "assets/images/rosa-sepanta-LzHrmneJJfA-unsplashmed.jpg",
+    alt: "meaningful description of my image",
+  },
 ];
 
-// Step 2: Create the thumbnail images
-// -Select the image containers from the DOM
+//Step 2: Create the thumbnail images
+// - Select the image containers from the DOM
 function createThumbnails(arrayOfImages) {
-  // We need a loop to create out thumbnails efficiently --> forEach, for loop
-  // in my loop, I have 4 steps
-  // create the dom element (img)
-  // update the attribute values to be the values of our images stored in the array (img.src = / img.alt = /img.className = )
-  // append the new imgs into the thumbnail container
-  // add an event listener here (event handler / event listener attached to each img --> img.addEventListener("click", function (){
-  // createFullScreenImage()}))
+  const thumbnailContainer = document.querySelector("#thumbnail-container"); // This is selecting the location that the images will go to, in this case the thumbnail container selection i made using HTML.
+
+  for (let i = 0; i < arrayOfImages.length; i++) {
+    // This is how to loop equal to the length of your array, initially this said "let i = 0; i < 3; i++" to loop three times, however this way it will add more loops if I hadd more images to my array of images
+    console.log(arrayOfImages[i]);
+
+    const imageElement = document.createElement("img"); // Here I am defining the imageElement Variable to create an image from my stored images data
+    imageElement.src = arrayOfImages[i].src; // This selects the source of image based on where it is currently at in the loop (due to the [i])
+    imageElement.className = "thumbnail-image"; // Applying a className so I can add CSS to the images
+    imageElement.addEventListener("click", function (mouseEvent) {
+      // Creating an event that is activated when I click on the image, to then pass to the createFullScreenImage function. Again the [i] lets the function know what image to use
+      createFullscreenImage(arrayOfImages[i]);
+    });
+    thumbnailContainer.appendChild(imageElement); //applying the function through appendchild. In other words adding the imageElement object to the thumbnailContainer
+  }
 }
 
-// Step 3: Create the full-screen image --> creating as user clicks on them no need to loop
-function createFullScreenImage(largeImg) {
-  // we need to clear the current full-screen image before we add a new one
-  fullScreenContainer.innerHTML = "";
-  // create a new image element (img)
-  // add a className
-  // set the src and alt values to be the same as the thumbnail that was clicked
-  // (image.src = largeImage.src)
-  // append this new image to the fullscreen container
+//Step 3: Create the full-screen image
+function createFullscreenImage(largeImage) {
+  // After relooking at Manny's puesdocode. I realise I missed a step when initially making this function. I did not know that he showed us how to clear the previous full screen image using "fullScreenContainer.innerHTML = "";"
+  // fullScreenContainer.innerHTML = ""; ---> This is good to know as it is much simpler than what I ended up doing.
+
+  const fullScreenContainer = document.querySelector("#full-screen-container"); //Same as the previous function, this is selecting the desired location from the document
+  const fullscreenImage = document.createElement("img"); // Creating the image within the section. As this is working off the previous function, it already knows which value to use.
+
+  // Instead, I used an if statement that starts at the beginning of the function and that selects any existing images in the container, and removes themn.
+
+  const oldImage = fullScreenContainer.querySelector("img");
+  if (oldImage) {
+    oldImage.remove();
+  }
+
+  // I think fullScreenContainer.innerHTML = ""; would be better to use in this situation. However the if statement may be more useful if you are looking to remove more than just an image?
+
+  fullscreenImage.className = "large-thumbnail-image"; // Adding a class name once again for CSS
+  fullscreenImage.alt = largeImage.alt;
+  fullscreenImage.src = largeImage.src; // Adding the alt and src from our stored images.
+  fullScreenContainer.appendChild(fullscreenImage); //adding the fullscreenImage to the fullscreenContainer
 }
+// Call create thumnails function using images supplied at the beginning
+createThumbnails(images);
